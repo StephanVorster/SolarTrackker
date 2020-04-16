6676cdb43859a18b457f090ebdb41243bdb4505a
u04579420 <u04579420@tuks.co.za>
2020-04-16 14:23:48 +0200

    Power Monitor:
    +Added power monitor library immperitive implementation of power monitor 
    class

---
+++ PowerMonitor/PowerMonitor/lib/PowerMonitor/PowerMonitor.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 2020/04/16.
+//
+
+#include "PowerMonitor.h"
---
+++ PowerMonitor/PowerMonitor/lib/PowerMonitor/PowerMonitor.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 2020/04/16.
+//
+
+#ifndef POWERMONITOR_POWERMONITOR_H
+#define POWERMONITOR_POWERMONITOR_H
+
+
+class PowerMonitor {
+
+};
+
+
+#endif //POWERMONITOR_POWERMONITOR_H
