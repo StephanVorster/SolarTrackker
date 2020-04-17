64d750ae257dab896d6636cec96f56a2c857b31d
u04579420 <u04579420@tuks.co.za>
2020-04-17 13:27:15 +0200

    Light Tracking:
    +Added timer library for timer interrupts on timer 1 
    +Added a timer with self added post scaler for 5  min interrupt call 
    ~ TODO: Comment code

--- LightTracking/LightTracking/src/main.cpp
+++ LightTracking/LightTracking/src/main.cpp
@@ -1,36 +1,59 @@
 #include "M28BYJ48.h"
 #include "LightSensor.h"
+#include "PowerMonitor.h"
+#include "TimerOne.h"
 
-void setup() {
-    Serial.begin(9600);
-}
+int counter = 0;
+float min = 5.00;
 
+PowerMonitor* monitor =  new PowerMonitor(A5);
 int mtmp[4] = {7,6,5,4};
 M28BYJ48* bottom = new M28BYJ48(M28BYJ48::V9,mtmp);
 int stmp[2] = {A3,A2};
 LightSensor* sen = new LightSensor(stmp);
 
+void timerIsr()
+{
+    if(counter >= int((min*60.00)/9)+1 ) {
+        monitor->displayPower();
+        counter = 0;
+    }
+    else {
+        counter++;
+    }
+}
+
+void setup() {
+
+    Serial.begin(9600);
+    Timer1.initialize(1000000*(600));
+    Timer1.attachInterrupt( timerIsr );
+}
+
+
+
+
 void loop() {
     int* val = sen->getSensorReadings();
     int diff = val[0]-val[1];
-    sen->printSensors();
+    //sen->printSensors();
 if( (diff > -15) && (diff < 15) ) {
-    Serial.println("Staying");
+    //Serial.println("Staying");
 }
 
 else {
     if(val[1] > val[0]){
 
         bottom->multiStepBackwards(10);
-        Serial.println("Moving Backwards");
+        //Serial.println("Moving Backwards");
 
         }
     else {
         bottom->multiStepForward(10);
-        Serial.println("Moving Forward");
+        //Serial.println("Moving Forward");
 
         }
     }
-    //delay(100);
+    delay(200);
 
 }
